2458e67aa1406b7502c92fe21aa5b5e2d62e277b
NarciSource <jwch11@gmail.com>
2024-11-27 15:58:51 +0900

    feat: Parcel 주입
    - Single-SPA

---
+++ src/pages/nav/ui/AuthControl.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    import { mountRootParcel } from "single-spa";
+
+    import { parcel as parcelConfig } from "@/../dist/parcel.js";
+
+    let container: HTMLDivElement;
+
+    onMount(() => {
+        const parcel = mountRootParcel(parcelConfig, {
+            domElement: container, // Svelte에서 React를 렌더링할 DOM 요소
+        });
+
+        return () => {
+            parcel.unmount();
+        };
+    });
+</script>
+
+<div bind:this={container}></div>
--- src/pages/nav/ui/NavTopBar.svelte
+++ src/pages/nav/ui/NavTopBar.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+
+    import AuthControl from "./AuthControl.svelte";
 </script>
 
 <TopAppBar variant="standard">
-    <Section>
-        <Row>
-            <Title>My App</Title>
+    <Row>
+        <Section>
+            <Title>PickMe</Title>
+        </Section>
+        <Section align="center">
             <nav>
                 <a href="/">Home</a>
-                <a href="/svelte">Svelte</a>
-                <a href="/vue">Vue</a>
-                <a href="/react">React</a>
+                <a href="/records">Records</a>
+                <a href="/calendar">Calendar</a>
+                <a href="/review">Review</a>
             </nav>
-        </Row>
-    </Section>
+        </Section>
+        <Section align="end">
+            <AuthControl />
+        </Section>
+    </Row>
 </TopAppBar>
