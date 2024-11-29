3879adf5c17c4d23caf392d096255297b77d865f
NarciSource <jwch11@gmail.com>
2024-11-29 22:43:25 +0900

    fix: NavTopBar 높이 할당
    - 아래 컴포넌트 가려짐

--- src/pages/nav/ui/NavTopBar.svelte
+++ src/pages/nav/ui/NavTopBar.svelte
@@ -5,26 +5,31 @@
     import AuthControl from "./AuthControl.svelte";
 </script>
 
-<TopAppBar variant="standard">
-    <Row>
-        <Section>
-            <Title tag="a" href="/">PickMe</Title>
-        </Section>
-        <Section align="center">
-            <nav>
-                <Button tag="a" href="/records" variant="unelevated">기록</Button>
-                <Button tag="a" href="/report" variant="unelevated">분석</Button>
-                <Button tag="a" href="/review" variant="unelevated">리뷰</Button>
-                <Button tag="a" href="/calendar" variant="unelevated">캘린더</Button>
-            </nav>
-        </Section>
-        <Section align="end">
-            <AuthControl />
-        </Section>
-    </Row>
-</TopAppBar>
+<div class="container">
+    <TopAppBar variant="standard">
+        <Row>
+            <Section>
+                <Title tag="a" href="/">PickMe</Title>
+            </Section>
+            <Section align="center">
+                <nav>
+                    <Button tag="a" href="/records" variant="unelevated">기록</Button>
+                    <Button tag="a" href="/report" variant="unelevated">분석</Button>
+                    <Button tag="a" href="/review" variant="unelevated">리뷰</Button>
+                    <Button tag="a" href="/calendar" variant="unelevated">캘린더</Button>
+                </nav>
+            </Section>
+            <Section align="end">
+                <AuthControl />
+            </Section>
+        </Row>
+    </TopAppBar>
+</div>
 
 <style>
+    .container {
+        height: 64px;
+    }
     :global(.mdc-top-app-bar) {
         --mdc-theme-primary: #3498db;
     }
