9cf22a5acf3bcc94f74b59ff46f9da815eddabe8
NarciSource <jwch11@gmail.com>
2024-12-19 18:57:46 +0900

    fix: '분석' 버튼의 링크를 '/reports'로 수정

--- src/pages/nav/ui/NavTopBar.svelte
+++ src/pages/nav/ui/NavTopBar.svelte
@@ -14,7 +14,7 @@
             <Section align="center">
                 <nav>
                     <Button tag="a" href="/records" variant="unelevated">기록</Button>
-                    <Button tag="a" href="/report" variant="unelevated">분석</Button>
+                    <Button tag="a" href="/reports" variant="unelevated">분석</Button>
                     <Button tag="a" href="/review" variant="unelevated">리뷰</Button>
                     <Button tag="a" href="/calendar" variant="unelevated">캘린더</Button>
                 </nav>
